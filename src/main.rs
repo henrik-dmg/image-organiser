b5d915c33f666d5aad8f2a6ba270fcb43d816585
Henrik Panhans <henrik@panhans.dev>
2023-03-15 21:27:07 +0100

    Nicer error reporting and stdout logging
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+use anyhow::{Context, Result};
 use clap::Parser;
+use log::info;
+use std::io::{self, Write};
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 struct CIArguments {
     configuration_path: std::path::PathBuf,
+    #[clap(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let stdout = io::stdout(); // get the global stdout entity
+    let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
+
     let args = CIArguments::parse();
+    let path = args.configuration_path.to_str().unwrap();
 
-    println!("Configuration path: {}", args.configuration_path.display());
-
-    let content = std::fs::read_to_string(&args.configuration_path)
-        .expect("Could not read configuration file.");
-
-    for line in content.lines() {
-        println!("{}", line);
-    }
+    let configuration_file_contents = std::fs::read_to_string(&args.configuration_path)
+        .with_context(|| format!("Could not read file \"{}\"", path))?;
+    writeln!(handle, "file content: {}", configuration_file_contents)?;
+    Ok(())
 }
