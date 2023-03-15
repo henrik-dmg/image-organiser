dff8cc4a9ea5a0a6566591d2a271b079d24c45cf
Henrik Panhans <henrik@panhans.dev>
2023-03-15 21:46:17 +0100

    Starting to pull code apart
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use log::info;
-use std::io::{self, Write};
+use std::io;
 
 #[derive(Debug, Parser)]
 struct CIArguments {
@@ -10,16 +9,27 @@ struct CIArguments {
     verbose: clap_verbosity_flag::Verbosity,
 }
 
+trait ImageOrganiser {
+    fn run_with_configuration_file(&self) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+impl ImageOrganiser for std::path::PathBuf {
+    fn run_with_configuration_file(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let path = self.to_str().unwrap();
+        let configuration_file_contents = std::fs::read_to_string(&self)
+            .with_context(|| format!("Could not read file \"{}\"", path))?;
+        println!("file content: {}", configuration_file_contents);
+        Ok(())
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let stdout = io::stdout(); // get the global stdout entity
     let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
 
     let args = CIArguments::parse();
-    let path = args.configuration_path.to_str().unwrap();
 
-    let configuration_file_contents = std::fs::read_to_string(&args.configuration_path)
-        .with_context(|| format!("Could not read file \"{}\"", path))?;
-    writeln!(handle, "file content: {}", configuration_file_contents)?;
+    args.configuration_path.run_with_configuration_file()?;
     Ok(())
 }
